b26212a47beb49a93171cbe8625c8401893e7362
Yordan Yordanov <y.yordanov90@gmail.com>
2021-12-09 22:16:21 +0200

    make Link connections in header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Header from "./components/Header";
+import Home from './components/Home';
+import Deals from './components/Deals';
+import Donate from './components/Donate';
+import Participants from './components/Participants';
+import Contact from './components/Contact';
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Footer from "./components/Footer";
 
 function App() {
 	return (
-		<div id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
-
+		<main id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
 			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/Deals" element={<Deals />} />
+				<Route path="/Donate" element={<Donate />} />
+				<Route path="/testimonial" element={<Participants />} />
+				<Route path="/contact" element={<Contact />} />
 
-			<Register />
-			<Login />
-
+			</Routes>
 			<Footer />
 
-
-			{/* <a href="#" id="scroll-to-top" className="hvr-radial-out"><i className="fa fa-angle-up"></i></a> */}
-		</div>
+		</main>
 	);
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <header className="top-header">
             <nav className="navbar header-nav navbar-expand-lg">
                 <div className="container">
-                    <a className="navbar-brand" href="index.html"><img src="images/logo.png" alt="image" /></a>
+                    <Link to="/" className="navbar-brand" ><img src="images/logo.png" alt="image" /></Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-wd"
                         aria-controls="navbar-wd" aria-expanded="false" aria-label="Toggle navigation">
                         <span></span>
@@ -12,12 +14,12 @@ const Header = () => {
                     </button>
                     <div className="collapse navbar-collapse justify-content-end" id="navbar-wd">
                         <ul className="navbar-nav">
-                            <li><a className="nav-link active" href="#home">Home</a></li>
-                            <li><a className="nav-link" href="#wish">Wish</a></li>
-                            <li><a className="nav-link" href="#b-deals">Best deals</a></li>
-                            <li><a className="nav-link" href="#donate">Donate</a></li>
-                            <li><a className="nav-link" href="#testi">Testimonial</a></li>
-                            <li><a className="nav-link" href="#contact">Contact</a></li>
+                            <li><Link className="nav-link active" to="/">Home</Link></li>
+                            <li><Link className="nav-link" to="/wish">Wish</Link></li>
+                            <li><Link className="nav-link" to="/deals">Best deals</Link></li>
+                            <li><Link className="nav-link" to="/donate">Donate</Link></li>
+                            <li><Link className="nav-link" to="/testimonial">Testimonial</Link></li>
+                            <li><Link className="nav-link" to="/contact">Contact</Link></li>
                         </ul>
                     </div>
                 </div>
