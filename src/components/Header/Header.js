2c6bf8a5ef25abb9c30d5d8596d37b4cde069db5
Yordan Yordanov <y.yordanov90@gmail.com>
2021-12-10 21:08:03 +0200

    separate guest and authenticated user views

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,32 +5,52 @@ const Header = ({
     user,
 }) => {
 
+    let userNavigation = (
+        <>
+            <li><Link className="nav-link" to="/deals">Best deals</Link></li>
+            <li><Link className="nav-link" to="/donate">Donate</Link></li>
+            <li><Link className="nav-link" to="/testimonial">Testimonial</Link></li>
+        </>
+    );
 
+    let guestAuthNavigation = (
+        <>
+            <li><Link className="nav-link" to="/login">Login</Link></li>
+            <li><Link className="nav-link" to="/register">Register</Link></li>
+        </>
+
+    );
+
+    let userAuthNavigation = (
+        <>
+            <span>Welcome, {user}</span>
+            <li><Link className="nav-link" to="/logout">Logout</Link></li>
+        </>
+    );
 
     return (
         <header className="top-header">
             <nav className="navbar header-nav navbar-expand-lg">
                 <div className="container">
                     <Link to="/" className="navbar-brand" ><img src="images/logo.png" alt="image" /></Link>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-wd"
+                    {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-wd"
                         aria-controls="navbar-wd" aria-expanded="false" aria-label="Toggle navigation">
                         <span></span>
                         <span></span>
                         <span></span>
-                    </button>
+                    </button> */}
                     <div className="collapse navbar-collapse justify-content-end" id="navbar-wd">
                         <ul className="navbar-nav">
                             <li><Link className="nav-link active" to="/">Home</Link></li>
                             <li><Link className="nav-link" to="/wish">Wish</Link></li>
-                            <li><Link className="nav-link" to="/deals">Best deals</Link></li>
-                            <li><Link className="nav-link" to="/donate">Donate</Link></li>
-                            <li><Link className="nav-link" to="/testimonial">Testimonial</Link></li>
+                            {isAuthenticated ? userNavigation : ''}
                             <li><Link className="nav-link" to="/contact">Contact</Link></li>
                         </ul>
                         <ul className="navbar-nav">
-                            <li><Link className="nav-link" to="/login">Login</Link></li>
-                            <li><Link className="nav-link" to="/register">Register</Link></li>
-                            <li><Link className="nav-link" to="/logout">Logout</Link></li>
+                            {isAuthenticated
+                                ? userAuthNavigation
+                                : guestAuthNavigation
+                            }
                         </ul>
                     </div>
                 </div>
