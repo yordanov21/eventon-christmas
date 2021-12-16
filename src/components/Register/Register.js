e252bd7382741d1a42e9cc2b615d15452456fe70
Yordan Yordanov <y.yordanov90@gmail.com>
2021-12-16 23:20:55 +0200

    Implement register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,16 +1,39 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router';
+
+import * as authService from '../../services/authService';
+import { AuthContext } from '../../contexts/AuthContext';
+
+
 const Register = () => {
 
+    const navigate = useNavigate();
+    const { login } = useContext(AuthContext);
+
+    const registerSubmitHandler = (e) => {
+        e.preventDefault();
+
+        let { email, password } = Object.fromEntries(new FormData(e.currentTarget));
+
+        authService.register(email, password)
+            .then(authData => {
+                login(authData);
+
+                navigate('/');
+            });
+    }
+
     return (
-        <div class="wrapper">
-            <form class="form-register">
-                <h2 class="form-register-heading">Register</h2>
-                <input type="text" class="form-control" name="email" placeholder="Email" required="" autofocus="" />
-                <input type="password" class="form-control" name="password" placeholder="Password" required="" />
-                <input type="password" class="form-control" name="repassword" placeholder="Re-Password" required="" />
-                <label class="checkbox">
+        <div className="wrapper">
+            <form id="register-form" method="POST" onSubmit={registerSubmitHandler}>
+                <h2 className="form-register-heading">Register</h2>
+                <input type="text" className="form-control" name="email" placeholder="Email" required="" autofocus="" />
+                <input type="password" className="form-control" name="password" placeholder="Password" required="" />
+                <input type="password" className="form-control" name="repassword" placeholder="Re-Password" required="" />
+                <label className="checkbox">
                     <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe" /> Remember me
                 </label>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
+                <button className="btn btn-lg btn-primary btn-block" type="submit">Register</button>
             </form>
         </div>
     );
--- src/services/authService.js
+++ src/services/authService.js
@@ -18,6 +18,17 @@ export const login = async (email, password) => {
     }
 };
 
+export const register = (email, password) => {
+    return fetch(`${baseUrl}/users/register`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    })
+        .then(res => res.json());
+};
+
 export const logout = () => {
     fetch(`${baseUrl}/users/logout`);
 };
