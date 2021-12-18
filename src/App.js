8cee93cfa30002418d4aac4c69369899c391021b
Yordan Yordanov <y.yordanov90@gmail.com>
2021-12-18 18:59:07 +0200

    Integrate ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -13,30 +13,34 @@ import Login from "./components/Login";
 import Logout from './components/Logout';
 import Register from "./components/Register";
 import Footer from "./components/Footer";
-
+import ErrorBoundary from './components/Common/ErrorBoundary';
+// bootstrap css 
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 	return (
-		<AuthProvider>
-			<main id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
-				<Header />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/logout" element={<Logout />} />
-					<Route path="/register" element={<Register />} />
-					<Route path="/deals" element={<Deals />} />
-					<Route path="/donate/*" element={<Donate />} />
-					<Route path="/create" element={<Create />} />
-					<Route path="/testimonial" element={<Participants />} />
-					<Route path="/contact" element={<Contact />} />
-					<Route path="/details/:cardId" element={<Details />} />
+		<ErrorBoundary>
+			<AuthProvider>
+				<main id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
+					<Header />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/logout" element={<Logout />} />
+						<Route path="/register" element={<Register />} />
+						<Route path="/deals" element={<Deals />} />
+						<Route path="/donate/*" element={<Donate />} />
+						<Route path="/create" element={<Create />} />
+						<Route path="/testimonial" element={<Participants />} />
+						<Route path="/contact" element={<Contact />} />
+						<Route path="/details/:cardId" element={<Details />} />
 
-				</Routes>
-				<Footer />
-			</main>
-		</AuthProvider>
+					</Routes>
+					<Footer />
+				</main>
+			</AuthProvider>
+		</ErrorBoundary>
+
 	);
 }
 
